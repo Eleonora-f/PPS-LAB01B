4f2fedfb49cff9318e7087a2c1fcb14babfce3ac
Eleonora_f <eleonora.falconi@studio.unibo.it>
2025-03-04 11:52:39 +0100

    refactor

--- src/main/java/e1/BronzeBankAccount.java
+++ src/main/java/e1/BronzeBankAccount.java
@@ -2,7 +2,9 @@ package e1;
 
 public class BronzeBankAccount implements BankAccount{
 
-    private BankAccount base;
+    public static final int WITHDRAW_FEE = 1;
+    public static final int WITHDRAW_FEE_THRESHOLD = 100;
+    private final BankAccount base;
 
     public BronzeBankAccount(BankAccount ba) {
         this.base = ba;
@@ -10,16 +12,19 @@ public class BronzeBankAccount implements BankAccount{
 
     @Override
     public int getBalance() {
-        return 0;
+        return base.getBalance();
     }
 
     @Override
     public void deposit(int amount) {
-
+        base.deposit(amount);
     }
 
     @Override
     public void withdraw(int amount) {
-
+        if(amount > base.getBalance()) {
+            throw new IllegalStateException();
+        }
+        base.withdraw(amount < WITHDRAW_FEE_THRESHOLD ? amount : amount + WITHDRAW_FEE);
     }
 }
